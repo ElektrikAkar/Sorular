c2ef9249b0ad33ce54b4b485befba6ab840d2f0d
Volkan Kumtepeli <volkan.kumtepeli@gmail.com>
2020-04-05 10:04:48 +0800

    bat dosyasi guncellendi.

--- convert_to_html.bat
+++ convert_to_html.bat
@@ -1,1 +1,5 @@
-pandoc main.tex -s --mathjax -o docs/index.html
+pdflatex.exe -synctex=1 -interaction=nonstopmode main.tex
+
+pandoc main.tex -s --mathjax -o docs/mobile.html
+
+pdf2htmlEX --zoom 1.5 --data-dir "C:\D\pdf2htmlEX-win32-0.14.6-upx-with-poppler-data\data" --dest-dir docs main.pdf index.html
